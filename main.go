d17b168c62beb06e9a789757c046fd7b145fa95e
Taras Shcherban <147646967+Miki535@users.noreply.github.com>
2024-06-17 16:43:28 +0300

    BackEnd

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"strings"
+)
+
 func main() {
-	
+	r := gin.Default()
+
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/", func(c *gin.Context) {})
+
+	r.GET("/tolower", func(c *gin.Context) {
+		c.HTML(200, "tolower.html", gin.H{})
+	})
+
+	r.POST("/tolower", func(c *gin.Context) {
+		info := c.PostForm("info")
+		result := strings.ToLower(info)
+		c.HTML(200, "tolower.html", gin.H{"result": result})
+	})
+
+	r.GET("/toupper", func(c *gin.Context) {
+		c.HTML(200, "toupper.html", gin.H{})
+	})
+
+	r.POST("/toupper", func(c *gin.Context) {
+		info := c.PostForm("info")
+		result := strings.ToUpper(info)
+		c.HTML(200, "toupper.html", gin.H{"result": result})
+	})
+
+	r.Run(":8080")
 }
